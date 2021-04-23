4c0859a57bf20c03ff9c49a1b6e293632e11bc7b
John Wan Ng <johnwanng@Johns-MBP.lan>
2021-04-23 09:23:31 +0800

    commit

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -22,92 +22,61 @@ function markerSize(magnitude) {
       return magnitude * 1500/600;
 }
 
-
-
-/* 
-* Create a circle symbol to use with a GeoJSON layer instead of the default blue marker
-*/
-
-
-/* function getColor(d) {
-return d > 5.7 ? '#800026' :
-       d > 5.5   ? '#BD0026' :
-       d > 5.3  ? '#E31A1C' :
-       d > 5.1  ? '#FC4E2A' :
-       d > 4.9   ? '#FD8D3C' :
-       d > 4.7   ? '#FEB24C' :
-       d > 4.5   ? '#FED976' :
-                  '#FFEDA0';
-}
-*/
-
-
+// Get color for magnitude
 function getColor(d) {
-return d > 10 ? '#800026' :
-       d > 5  ? '#BD0026' :
-       d > 4  ? '#E31A1C' :
-       d > 3  ? '#FC4E2A' :
-       d > 2  ? '#FD8D3C' :
-       d > 1  ? '#FEB24C' :
-                '#FED976';
-                //'#FFEDA0';
+  return d > 5 ? '#800026' :
+        d > 4  ? '#BD0026' :
+        d > 3  ? '#E31A1C' :
+        d > 2  ? '#FC4E2A' :
+        d > 1  ? '#FD8D3C' :
+                  '#FEB24C';
 }
 // This will be run when L.geoJSON creates the point layer from the GeoJSON data.
 function createCircleMarker( feature, latlng ){
-//console.log(feature.properties.mag);
-//if (feature.properties.mag <= 5) {colour = "Yellow"}
-colour = getColor(feature.properties.mag)
-/* if (feature.properties.mag <= 4.6) {colour = "#f0fc03"}
-else if (feature.properties.mag <= 4.8) {colour = "#fcf803"}
-else if (feature.properties.mag <= 5) {colour = "#fceb03"}
-else if (feature.properties.mag <= 5.2) {colour = "#fce303"}
-else if (feature.properties.mag <= 5.4) {colour = "#fcdb03"}
-else if (feature.properties.mag <= 5.6) {colour = "#fcc603"}
-else if (feature.properties.mag <= 5.8) {colour = "#fcb103"}
-else if (feature.properties.mag <= 6) {colour = "#fc9003"}
-else if (feature.properties.mag <= 6.2) {colour = "#fc6b03"}
-else if (feature.properties.mag <= 6.4) {colour = "#fc4a03"} */
+  
+  // Decide the color from the magnitube
+  colour = getColor(feature.properties.mag)
 
-// Change the values of these options to change the symbol's appearance
-var options = {
-  radius: markerSize(feature.properties.mag),
-  fillColor: colour,
-  color: "black",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8
-}
+  // Change the values of these options to change the symbol's appearance
+  var options = {
+    radius: markerSize(feature.properties.mag),
+    fillColor: colour,
+    color: "black",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  }
 
-var markerInfo = 'Location: ' + feature.properties.place + '<br>' + 'Magnitude: ' + feature.properties.mag;
-
-return L.circleMarker( latlng, options ).addTo(myMap).bindPopup(markerInfo);
-//}
+  // Prepare mark info for the earthquake location
+  var markerInfo = 'Location: ' + feature.properties.place + '<br>' + 'Magnitude: ' + feature.properties.mag + colour;
+  // Add marker to map
+  return L.circleMarker( latlng, options ).addTo(myMap).bindPopup(markerInfo);
 }
 
 
 // Grabbing our GeoJSON data..
 d3.json(url, function(data) {
+  
   // Creating a GeoJSON layer with the retrieved data
-
   L.geoJSON( data, {
     pointToLayer: createCircleMarker // Call the function createCircleMarker to create the symbol for this layer
   }).addTo( myMap )
 
-
-
-var legend = L.control({position: 'bottomright'});
-  legend.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'info legend'),
-          magnitude = [0, 1, 2, 3, 4, 5],
-          labels = [];
-  
-      // loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < magnitude.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + getColor(magnitude[i] + 1) + '"></i> ' +
-              magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
-      }
-      return div;
-  };
-  legend.addTo(myMap);
+  // Define the legend 
+  var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            magnitude = [0, 1, 2, 3, 4, 5],
+            labels = [];
+    
+        // loop through our magnitude intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < magnitude.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(magnitude[i] + 1) + '"></i> ' +
+                magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+    //Add Legend to map 
+    legend.addTo(myMap);
 });
